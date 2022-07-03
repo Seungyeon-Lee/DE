4af16d2eec30ec89861fcbce0d8e9cae7d5fb797
SeungYeon <yeondiary@gmail.com>
2022-07-04 00:16:59 +0900

    Camera 추가.

---
+++ DXEngine/Graphics/VECamera.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "../VEInclude.h"
+#include "../Math/VEVector3.h"
+#include "../Math/VEMatrix4.h" 
+
+
+namespace Venus
+{
+    class VELinearColor;
+    class VE_API VECamera
+    {
+    public:
+        VECamera();
+
+        void SetupViewMatrix(const VEVector3& pos,
+            const VEVector3& targetPos,
+            const VEVector3& worldUpDir);
+
+        void SetPerspective(float fov, float aspect, float near, float far);
+
+        float Near() const { return near; }
+        float Far() const { return far; }
+        VEVector3 Position() const { return position; }
+        VEMatrix4 ViewMatrix() const { return viewMatrix; }
+        VEMatrix4 ProjectionMatrix() const { return projectionMatrix; }
+
+    private:
+        float near;
+        float far;
+        VEVector3 position;
+        VEMatrix4 viewMatrix;
+        VEMatrix4 projectionMatrix;
+    };
+}
